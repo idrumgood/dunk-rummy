bb6cdfd20cf1ecfc2140653d5dd3528eb2ffafc4
Bryan Dunk <idrumgood@gmail.com>
2022-06-13 17:29:49 -0500

    setup game entry form

--- src/pages/AddGame.vue
+++ src/pages/AddGame.vue
@@ -1,7 +1,14 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, onMounted, ref } from 'vue';
 
 let allPlayers = reactive([]);
+let player1 = ref();
+let player1Score = ref(0);
+let player1HandWins = ref(0);
+let player2 = ref();
+let player2Score = ref(0);
+let player2HandWins = ref(0);
+
 const getAllPlayers = async (e) => {
   try {
     let response = await fetch('/.netlify/functions/get-all-players');
@@ -11,20 +18,61 @@ const getAllPlayers = async (e) => {
      console.error(err);
   }
 }
+
+const submitGame = () => console.log('Game submitted');
+
+onMounted(async () => {
+  await getAllPlayers();
+});
 </script>
 
 <template>
-  <v-card>
-    <v-form>
-      <v-card-title>Create a new game</v-card-title>
+  <v-form>
+    <h1>Create a new game</h1>
+    <v-card>
+      <v-card-title>Player 1</v-card-title>
       <v-card-text>
-        <p v-for="player in allPlayers">{{player.name}} has ID {{player.id}}</p>
+        <v-select
+          v-model="player1"
+          :items="allPlayers"
+          item-title="name"
+          item-value="id"
+          label="Who was player 1?"
+        ></v-select>
+        <v-text-field
+          v-model="player1Score"
+          label="What was player 1's score?"
+        ></v-text-field>
+        <v-text-field
+          v-model="player1HandWins"
+          label="How many hand wins did Player 1 have?"
+        ></v-text-field>
+      </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-title>Player 2</v-card-title>
+      <v-card-text>
+        <v-select
+          v-model="player2"
+          :items="allPlayers"
+          item-title="name"
+          item-value="id"
+          label="Who was player 2?"
+        ></v-select>
+        <v-text-field
+          v-model="player2Score"
+          label="What was player 2's score?"
+        ></v-text-field>
+        <v-text-field
+          v-model="player2HandWins"
+          label="How many hand wins did Player 2 have?"
+        ></v-text-field>
       </v-card-text>
       <v-card-actions>
-        <v-btn @click="getAllPlayers">Get all players</v-btn> 
+        <v-btn @click="submitGame">Get all players</v-btn> 
       </v-card-actions>
-    </v-form>
-  </v-card>
+    </v-card>
+  </v-form>
 </template>
 
 <style scoped>
